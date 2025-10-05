5e2ef058490fa5a6cc107c144e5acd7feff8f414
Dmytro Denysov <dmytro.denysov@danads.se>
2025-10-05 11:02:45 +0300

    PET-3: Implement Purrsurance Chat Interface + Fixed header

--- apps/chat/app/layouts/default.vue
+++ apps/chat/app/layouts/default.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="default-layout">
+   <AppHeader />
+
     <main>
       <slot />
     </main>
     
-    <footer>
-      <p>TBD: Application Footer</p>
-    </footer>
+    <AppFooter />
   </div>
 </template>
 
 <script setup lang="ts">
 // TBD: Add layout logic here
+import AppHeader from "~/components/layout/AppHeader.vue";
+import AppFooter from "~/components/layout/AppFooter.vue";
 </script>
 
 <style scoped>
