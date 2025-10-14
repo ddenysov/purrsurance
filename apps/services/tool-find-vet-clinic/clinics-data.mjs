722b9eb48315dbfa4c7c982a788f2c88564b2c70
Dmytro Denysov <dmytro.denysov@danads.se>
2025-10-14 18:45:14 +0300

    PET-13 Code reorganization

---
+++ apps/services/tool-find-vet-clinic/clinics-data.mjs
@@ -0,0 +1,288 @@
+/**
+ * Mock vet clinics data
+ * Contains mock data for 20 veterinary clinics across Ukraine
+ */
+
+export const mockClinics = [
+  {
+    id: "CLINIC-LV-001",
+    name: "Pet Care Center Lviv",
+    address: {
+      street: "Lychakivska St, 78",
+      city: "Lviv",
+      postalCode: "79008",
+      country: "Ukraine"
+    },
+    phone: "+380322345678",
+    email: "contact@petcarelviv.ua",
+    specialty: "Orthopedics",
+    acceptsInsurance: true
+  },
+  {
+    id: "CLINIC-KV-001",
+    name: "VetLife Clinic Kyiv",
+    address: {
+      street: "Khreshchatyk St, 22",
+      city: "Kyiv",
+      postalCode: "01001",
+      country: "Ukraine"
+    },
+    phone: "+380442234567",
+    email: "info@vetlife.kiev.ua",
+    specialty: "General Practice",
+    acceptsInsurance: true
+  },
+  {
+    id: "CLINIC-OD-001",
+    name: "Animal Hospital Odesa",
+    address: {
+      street: "Deribasivska St, 15",
+      city: "Odesa",
+      postalCode: "65026",
+      country: "Ukraine"
+    },
+    phone: "+380482345678",
+    email: "care@animalhospital.odesa.ua",
+    specialty: "Emergency & Critical Care",
+    acceptsInsurance: true
+  },
+  {
+    id: "CLINIC-KV-002",
+    name: "Happy Paws Veterinary",
+    address: {
+      street: "Velyka Vasylkivska St, 100",
+      city: "Kyiv",
+      postalCode: "03150",
+      country: "Ukraine"
+    },
+    phone: "+380443456789",
+    email: "hello@happypaws.kiev.ua",
+    specialty: "Dentistry",
+    acceptsInsurance: true
+  },
+  {
+    id: "CLINIC-LV-002",
+    name: "Paw & Claw Clinic",
+    address: {
+      street: "Shevchenka Ave, 45",
+      city: "Lviv",
+      postalCode: "79000",
+      country: "Ukraine"
+    },
+    phone: "+380322456789",
+    email: "info@pawclaw.lviv.ua",
+    specialty: "Surgery",
+    acceptsInsurance: false
+  },
+  {
+    id: "CLINIC-DN-001",
+    name: "Dnipro Veterinary Center",
+    address: {
+      street: "Haharina Ave, 56",
+      city: "Dnipro",
+      postalCode: "49000",
+      country: "Ukraine"
+    },
+    phone: "+380562567890",
+    email: "contact@vetcenter.dnipro.ua",
+    specialty: "Cardiology",
+    acceptsInsurance: true
+  },
+  {
+    id: "CLINIC-KH-001",
+    name: "Kharkiv Animal Care",
+    address: {
+      street: "Sumska St, 89",
+      city: "Kharkiv",
+      postalCode: "61000",
+      country: "Ukraine"
+    },
+    phone: "+380577890123",
+    email: "info@animalcare.kharkiv.ua",
+    specialty: "Dermatology",
+    acceptsInsurance: true
+  },
+  {
+    id: "CLINIC-OD-002",
+    name: "Sea Breeze Vet Clinic",
+    address: {
+      street: "Frantsuzkyi Blvd, 77",
+      city: "Odesa",
+      postalCode: "65009",
+      country: "Ukraine"
+    },
+    phone: "+380487891234",
+    email: "info@seabreezevet.ua",
+    specialty: "Exotic Animals",
+    acceptsInsurance: false
+  },
+  {
+    id: "CLINIC-ZP-001",
+    name: "Zaporizhzhia Pet Hospital",
+    address: {
+      street: "Soborny Ave, 123",
+      city: "Zaporizhzhia",
+      postalCode: "69000",
+      country: "Ukraine"
+    },
+    phone: "+380617123456",
+    email: "hospital@zppet.ua",
+    specialty: "Internal Medicine",
+    acceptsInsurance: true
+  },
+  {
+    id: "CLINIC-KV-003",
+    name: "Capital Veterinary Clinic",
+    address: {
+      street: "Peremohy Ave, 50",
+      city: "Kyiv",
+      postalCode: "03057",
+      country: "Ukraine"
+    },
+    phone: "+380449876543",
+    email: "contact@capitalvet.kiev.ua",
+    specialty: "Oncology",
+    acceptsInsurance: true
+  },
+  {
+    id: "CLINIC-LV-003",
+    name: "West Pet Medical Center",
+    address: {
+      street: "Horodotska St, 200",
+      city: "Lviv",
+      postalCode: "79040",
+      country: "Ukraine"
+    },
+    phone: "+380326543210",
+    email: "info@westpet.lviv.ua",
+    specialty: "Neurology",
+    acceptsInsurance: true
+  },
+  {
+    id: "CLINIC-KH-002",
+    name: "Veterinary Excellence Kharkiv",
+    address: {
+      street: "Nauky Ave, 17",
+      city: "Kharkiv",
+      postalCode: "61166",
+      country: "Ukraine"
+    },
+    phone: "+380575432109",
+    email: "excellence@vetkharkiv.ua",
+    specialty: "Ophthalmology",
+    acceptsInsurance: false
+  },
+  {
+    id: "CLINIC-VN-001",
+    name: "Vinnytsia Animal Clinic",
+    address: {
+      street: "Soborna St, 68",
+      city: "Vinnytsia",
+      postalCode: "21000",
+      country: "Ukraine"
+    },
+    phone: "+380432654321",
+    email: "info@animalclinic.vn.ua",
+    specialty: "General Practice",
+    acceptsInsurance: true
+  },
+  {
+    id: "CLINIC-PL-001",
+    name: "Poltava Veterinary Services",
+    address: {
+      street: "Sobornosti St, 32",
+      city: "Poltava",
+      postalCode: "36000",
+      country: "Ukraine"
+    },
+    phone: "+380532765432",
+    email: "services@vetpoltava.ua",
+    specialty: "Reproductive Medicine",
+    acceptsInsurance: true
+  },
+  {
+    id: "CLINIC-KV-004",
+    name: "Pet Wellness Center Kyiv",
+    address: {
+      street: "Obolon Ave, 28",
+      city: "Kyiv",
+      postalCode: "04205",
+      country: "Ukraine"
+    },
+    phone: "+380448765432",
+    email: "wellness@petwellness.kiev.ua",
+    specialty: "Preventive Care",
+    acceptsInsurance: true
+  },
+  {
+    id: "CLINIC-IF-001",
+    name: "Carpathian Pet Clinic",
+    address: {
+      street: "Nezalezhnosti St, 55",
+      city: "Ivano-Frankivsk",
+      postalCode: "76000",
+      country: "Ukraine"
+    },
+    phone: "+380342876543",
+    email: "info@carpathianpet.if.ua",
+    specialty: "Emergency & Critical Care",
+    acceptsInsurance: true
+  },
+  {
+    id: "CLINIC-OD-003",
+    name: "Black Sea Veterinary",
+    address: {
+      street: "Preobrazhenska St, 90",
+      city: "Odesa",
+      postalCode: "65000",
+      country: "Ukraine"
+    },
+    phone: "+380489876543",
+    email: "contact@blackseavet.ua",
+    specialty: "Surgery",
+    acceptsInsurance: true
+  },
+  {
+    id: "CLINIC-DN-002",
+    name: "River City Animal Hospital",
+    address: {
+      street: "Naberezhna Peremohy St, 44",
+      city: "Dnipro",
+      postalCode: "49094",
+      country: "Ukraine"
+    },
+    phone: "+380567654321",
+    email: "hospital@rivercity.dnipro.ua",
+    specialty: "Orthopedics",
+    acceptsInsurance: false
+  },
+  {
+    id: "CLINIC-LT-001",
+    name: "Lutsk Veterinary Complex",
+    address: {
+      street: "Volodymyrska St, 12",
+      city: "Lutsk",
+      postalCode: "43000",
+      country: "Ukraine"
+    },
+    phone: "+380332543210",
+    email: "complex@vetlutsk.ua",
+    specialty: "General Practice",
+    acceptsInsurance: true
+  },
+  {
+    id: "CLINIC-KV-005",
+    name: "Metropolitan Pet Care",
+    address: {
+      street: "Lesi Ukrainky Blvd, 26",
+      city: "Kyiv",
+      postalCode: "01133",
+      country: "Ukraine"
+    },
+    phone: "+380441234567",
+    email: "care@metropet.kiev.ua",
+    specialty: "Behavioral Medicine",
+    acceptsInsurance: true
+  }
+];
+
